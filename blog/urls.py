c980a23a9096dd78f915a341447e6400e1d62f9e
Nastya <1360nastya@mail.ru>
2025-03-05 09:46:23 +0300

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path 
 from . import views
 
-#urlpatterns = [
-#    path('',views.post,name='post')
-#]
-
+urlpatterns = [
+    path('/',views.post,name='post')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    pass
 def post_list(request):
     return render(request,'blog/post_list.html',{})
 def post(request):
