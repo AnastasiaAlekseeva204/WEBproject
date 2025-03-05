43b1ad5feb08d9d03644904a2823bcc99aed7deb
Nastya <1360nastya@mail.ru>
2025-03-05 09:06:41 +0300

    my first project create

---
+++ blog/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Post 
+from .models import Book
+from .models import Author
+from .models import Rubric
+from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
+from .models import Category
+
+admin.site.register(
+    Category,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Book)
+admin.site.register(Author)
+#admin.site.register(Rubric,MPTTModelAdmin)
---
+++ blog/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Category(MPTTModel):
+    id = models.AutoField(primary_key=True)
+    parent = TreeForeignKey('self',on_delete=models.CASCADE,null=True,blank=True,related_name='children')
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True)
+    enabled = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+class Rubric(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+        
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    id_rubric = TreeForeignKey(Rubric,on_delete=models.PROTECT,null=True)
+    text = models.TextField(null=True)
+    author = models.CharField(max_length=100)
+    create_data = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id)+". "+self.title
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = TreeForeignKey(Category,on_delete=models.PROTECT,null=True)
+    title = models.CharField(max_length=100)
+    #id_author = models.IntegerField()
+    id_author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    price = models.IntegerField(null=True)
+    content = models.TextField(null=True)
+    data_pub = models.DateTimeField(default=timezone.now)
+    enabled = models.BooleanField()
+    reiting = models.PositiveSmallIntegerField(null=True)
+    
+    def __str__(self):
+        return str(self.id)+". "+self.title
+
+
+    
