8b7a209b43f91556037bb58b8c982506425566f7
Nastya <1360nastya@mail.ru>
2025-03-12 10:29:58 +0300

    add author

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post,name='post')
+    path('',views.main,name='main'),
+    path('book',views.book,name='book'),
+    path('author',views.author,name='author'),
+    path('category',views.category,name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from .models import Book
+from .models import Category
+from .models import Author
 # Create your views here.
 
-def post(request):
+def main(request):
     books= Book.objects.all()
-    return render(request,'blog/post.html',{'books':books})
+    authors = Author.objects.all()
+    categorys = Category.objects.filter(parent__isnull=True).all()
+    return render(request,'blog/main.html',{'books':books,'categorys':categorys,'authors': authors})
+
+def book(request):
+    book_id = request.GET.get('book_id')
+    #category = Category.objects.get(id=cat_id)
+    #books = Book.objects.filter(category=cat_id).all()
+    #return render(request,'blog/book.html',{'books':books,'category':category})
+    return render(request,'blog/book.html',{})
+
+def author(request):
+    author_id = request.GET.get('author_id')
+    author=Author.objects.get(id=author_id)
+    books = Book.objects.filter(id_author=author_id).all()
+    return render(request,'blog/author.html',{'books':books,'author':author})
+
+def category(request):
+    cat_id = request.GET.get('cat_id')
+    category = Category.objects.get(id=cat_id)
+    books = Book.objects.filter(category=cat_id).all()
+    return render(request,'blog/category.html',{'books':books,'category':category})
